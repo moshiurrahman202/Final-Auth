c39453449cebfe87691c99b17cd091d2fdf3f350
Mosiur Rahman sumon <mosiurrahman123654@gmail.com>
2025-10-31 19:15:43 +0600

    routing nav and fooret and outlet done

--- src/Laout/Root.jsx
+++ src/Laout/Root.jsx
@@ -1,10 +1,14 @@
 import { Outlet } from "react-router";
+import Navbar from "../Components/Nav/Navbar";
+import Footer from "../Components/Footer/Footer";
 
 
 const Root = () => {
     return (
         <div>
+            <Navbar></Navbar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,16 @@ import { createRoot } from 'react-dom/client'
 import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
 import './index.css'
+import Root from './Laout/Root';
+import Home from './Components/Home/Home';
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello World</div>,
+    Component: Root,
+    children: [
+      {index: true, Component: Home}
+    ]
   },
 ]);
 createRoot(document.getElementById('root')).render(
