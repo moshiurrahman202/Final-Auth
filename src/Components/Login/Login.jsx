1da400c03ebe9c96ee6e125f59418a137f56299b
Mosiur Rahman sumon <mosiurrahman123654@gmail.com>
2025-11-03 20:13:42 +0600

    pass reset

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,8 +1,10 @@
-import { signInWithEmailAndPassword } from "firebase/auth";
+import { signInWithEmailAndPassword, sendPasswordResetEmail  } from "firebase/auth";
 import { auth } from "../../Config/__config__final__auth";
 import { Link } from "react-router";
+import { useRef } from "react";
 
 const Login = () => {
+    const emailRef = useRef()
     const loginformhandle = e => {
         e.preventDefault()
         console.log("clicked => login=>");
@@ -15,6 +17,9 @@ const Login = () => {
             console.log("user => done=>",res);
             if(!res.user.emailVerified){
                 alert("Please verify your email address!!")
+            }else{
+                console.log("login successful => ❤");
+                
             }
             
         })
@@ -25,17 +30,29 @@ const Login = () => {
 
         
     } 
+    const forgetpasshandler = () => {
+        const email = emailRef.current.value;
+        sendPasswordResetEmail(auth, email)
+        .then(() => {
+            alert("check your email we sent a email that can you reset your password")
+        })
+        .catch(error => {
+            console.log(error.message);
+            
+        })
+        
+    }
     return (
         <div className="flex justify-center">
             <form onSubmit={loginformhandle} className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
   <legend className="fieldset-legend text-2xl">Login now!</legend>
 
   <label className="label">Email</label>
-  <input name="email" type="email" className="input" placeholder="Email" />
+  <input name="email" type="email" ref={emailRef} className="input" placeholder="Email" />
 
   <label className="label">Password</label>
   <input name="pass" type="password" className="input" placeholder="Password" />
-        <div><a className="link link-hover">Forget Password?</a></div>
+        <div onClick={forgetpasshandler}><a className="link link-hover">Forget Password?</a></div>
   <button className="btn btn-neutral mt-4">Login</button>
   <div><p>New to this website? please <Link to="/signup"><button className="link link-hover text-blue-600"> SignUp</button></Link></p></div>
 </form>
