b66507505aa56fb42b535315c1af32771961fa5b
Mosiur Rahman sumon <mosiurrahman123654@gmail.com>
2025-11-16 13:20:25 +0600

    signup with authprovider

--- src/Components/Signup/Signup.jsx
+++ src/Components/Signup/Signup.jsx
@@ -1,116 +1,117 @@
-import { useState } from "react";
-import { createUserWithEmailAndPassword, sendEmailVerification, updateProfile  } from "firebase/auth";
+import { use, useState } from "react";
+import { sendEmailVerification, updateProfile } from "firebase/auth";
 import { auth } from "../../Config/__config__final__auth";
 import { Link } from "react-router";
+import { AuthContext } from "../../Context/AuthContext";
 
 
 const Signup = () => {
-    const [errorMessage, setErrorMessage] = useState("")
-    const [done, setDone] = useState(false)
-    const [showpass, setShowpass] = useState(false)
-    
+  const [errorMessage, setErrorMessage] = useState("")
+  const [done, setDone] = useState(false)
+  const [showpass, setShowpass] = useState(false)
+  const { createUser } = use(AuthContext)
+  console.log(createUser);
+  
 
-    const handleform = e => {
-        e.preventDefault()
-        setErrorMessage("")
-        setDone(false)
-        const name = e.target.name.value
-        const photo = e.target.photo.value
-        const email = e.target.email.value
-        const pass = e.target.pass.value
-        const checked = e.target.checkbox.checked
-        // console.log("box=> ", checked);
-        // console.log("clicked => ",email, pass);
-        
-        const pass_pattern = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/
-        if(pass_pattern.test(pass) === false){
-            setErrorMessage("Must be more than 6 characters, including number, lowercase letter, uppercase letter")
 
-        }
-        if(!checked){
-          setErrorMessage("Plz accept trms ans conditions")
-          return;
-        }
-        createUserWithEmailAndPassword(auth, email, pass)
-        .then(userCredential => {
-            
-            const user = userCredential.user;
-            console.log(user);
-            // email varify
-            sendEmailVerification(auth.currentUser)
-            .then(() => {
-              setDone(true)
-              const profile = {
-                displayName: name,
-                photoURL: photo
-              }
-              updateProfile(auth.currentUser, profile)
+  const handleform = e => {
+    e.preventDefault()
+    setErrorMessage("")
+    setDone(false)
+    const name = e.target.name.value
+    const email = e.target.email.value
+    const pass = e.target.pass.value
+    const checked = e.target.checkbox.checked
+    // console.log("box=> ", checked);
+    // console.log("clicked => ",email, pass);
+
+    const pass_pattern = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/
+    if (pass_pattern.test(pass) === false) {
+      setErrorMessage("Must be more than 6 characters, including number, lowercase letter, uppercase letter")
+
+    }
+    if (!checked) {
+      setErrorMessage("Plz accept trms ans conditions")
+      return;
+    }
+    // createUserWithEmailAndPassword(auth, email, pass)
+       createUser(email, pass)
+      .then(userCredential => {
+
+        const user = userCredential.user;
+        console.log(user);
+        // email varify
+        sendEmailVerification(auth.currentUser)
+          .then(() => {
+            setDone(true)
+            const profile = {
+              displayName: name
+            }
+            updateProfile(auth.currentUser, profile)
               .then(() => {
                 console.log("profile updated!");
-                
+
               })
               .catch(erroe => {
                 console.log(erroe);
-                
+
               })
-            })
-            .catch(error => {
-              console.log(error);
-              
-            })
-            
-        })
-        .catch(error => {
+          })
+          .catch(error => {
             console.log(error);
-            setErrorMessage(error.message)
 
-            
-        })
+          })
 
-    }
+      })
+      .catch(error => {
+        console.log(error);
+        setErrorMessage(error.message)
 
 
-    return (
-        <div>
-            <div className="hero bg-base-200 min-h-screen">
-  <div className="hero-content flex-col lg:flex-row-reverse">
-    <div className="text-center lg:text-left">
-      <h1 className="text-5xl font-bold">SignUp now!</h1>
-      <p className="py-6">
-        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
-        quasi. In deleniti eaque aut repudiandae et a id nisi.
-      </p>
-    </div>
-    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-      <div className="card-body">
-        <form className="fieldset" onSubmit={handleform}>
-          <label className="label">Name</label>
-          <input type="text" name="name" className="input" placeholder="Your Name" />
-          <label className="label">Photo</label>
-          <input type="text" name="photo" className="input" placeholder="Photo URL" />
-          <label className="label">Email</label>
-          <input type="email" name="email" className="input" placeholder="Email" />
-          <div className="relative">
-            <label className="label">Password</label>
-          <input name="pass" type={showpass ? "text" : "password"} className="input" placeholder="Password" />
-          <button onClick={() => setShowpass(!showpass)} className="absolute top-6.5 right-6">👁‍🗨</button>
+      })
+
+  }
+
+
+  return (
+    <div>
+      <div className="hero bg-base-200 min-h-screen">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="text-center lg:text-left">
+            <h1 className="text-5xl font-bold">SignUp now!</h1>
+            <p className="py-6">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+              quasi. In deleniti eaque aut repudiandae et a id nisi.
+            </p>
           </div>
-          <p><input name="checkbox" type="checkbox" className="checkbox" />   accept terms and conditions</p>
-          {
-            errorMessage && <div><p className="text-red-600">{errorMessage}</p></div>
-          }
-          {
-             done && <div><p className="text-green-600">It's been successful, and we've sent an email that we can verify you.</p></div>
-          }
-          <button className="btn btn-neutral mt-4">SignUp</button>
-        </form>
-        <div><p>Do you already have an account?  <Link to="/login"><a className="link link-hover text-blue-600"> Login</a></Link></p></div>
+          <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+            <div className="card-body">
+              <form className="fieldset" onSubmit={handleform}>
+                <label className="label">Name</label>
+                <input type="text" name="name" className="input" placeholder="Your Name" />
+                <label className="label">Email</label>
+                <input type="email" name="email" className="input" placeholder="Email" />
+                <div className="relative">
+                  <label className="label">Password</label>
+                  <input name="pass" type={showpass ? "text" : "password"} className="input" placeholder="Password" />
+                  <button onClick={() => setShowpass(!showpass)} className="absolute top-6.5 right-6">👁‍🗨</button>
+                </div>
+                <p><input name="checkbox" type="checkbox" className="checkbox" />   accept terms and conditions</p>
+                {
+                  errorMessage && <div><p className="text-red-600">{errorMessage}</p></div>
+                }
+                {
+                  done && <div><p className="text-green-600">It's been successful, and we've sent an email that we can verify you.</p></div>
+                }
+                <button className="btn btn-neutral mt-4">SignUp</button>
+              </form>
+              <div><p>Do you already have an account?  <Link to="/login"><a className="link link-hover text-blue-600"> Login</a></Link></p></div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-</div>
-        </div>
-    );
+  );
 };
 
 export default Signup;
--- src/Context/AuthProvider.jsx
+++ src/Context/AuthProvider.jsx
@@ -1,14 +1,17 @@
+import { auth } from "../Config/__config__final__auth";
 import { AuthContext } from "./AuthContext";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 
 
 const AuthProvider = ({children}) => {
-    const userinfo = {
-        name: "sumon",
-        age: "34",
-        add: "c&b colony"
+    const createUser = (email, pass) => {
+        return createUserWithEmailAndPassword(auth, email, pass)
+    }
+    const userInfo = {
+        createUser,
     }
     return (
-        <AuthContext value={userinfo}>{children}</AuthContext>
+        <AuthContext value={userInfo}>{children}</AuthContext>
     );
 };
 
